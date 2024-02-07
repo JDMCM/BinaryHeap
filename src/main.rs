2e3e3281ed4723c4e20e39b1672d1d034dfea211
JDMCM <jmcmahon@trinity.edu>
2024-02-06 20:02:39 -0600

    I did this while sick (sick af)

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,13 @@ fn main() {
 
 struct Bheap<T:Copy, F:Fn(T,T)->bool> {
     pub data: Vec<T>,
-    pub length: usize,
     pub predicate: F,
     
 }
 
-impl<T:Copy, F:Fn(T,T)->bool> Bheap<T,F> {
+impl<T:Copy, F: Fn(T,T)->bool> Bheap<T,F> {
     fn new(pred: F) -> Self {
-        return Self {data: Vec::new(), length: 0, predicate: pred}
-    }
-
-    fn with_capacity(pred: F, size: usize) -> Self {
-        return Self {data: Vec::with_capacity(size), length: size, predicate: pred}
+        return Self {data: Vec::new(), predicate: pred}
     }
 
     fn top(child: usize) -> usize {
@@ -31,13 +26,58 @@ impl<T:Copy, F:Fn(T,T)->bool> Bheap<T,F> {
     }
 
     fn fixup(&mut self) {
-        let rover: usize = self.length;
+        let mut rover: usize = self.data.len();
         let mut next: usize =Self::top(rover);
-        let tmp = self.data[rover];
-        self.data[next] = tmp
-        
-
+        let mut tmp = self.data[rover];
+        while rover != 0 && (self.predicate)(self.data[rover],self.data[rover]) {
+            tmp = self.data[rover];
+            self.data[rover] = self.data[next];
+            self.data[next] = tmp;
+            rover = next;
+            next = Self::top(rover);
+        }
     }
 
+    fn fixdown(&mut self) {
+        let mut rover:usize = 0;
+        let mut tmp = self.data[rover];
+        while Self::left(rover) < self.data.len() &&
+         ((self.predicate)(self.data[Self::left(rover)],self.data[rover]) || 
+         (self.predicate)(self.data[Self::right(rover)],self.data[rover])) {
+            tmp = self.data[rover];
+            if (self.predicate)(self.data[Self::left(rover)],self.data[Self::right(rover)]) {
+                self.data[rover] = self.data[Self::left(rover)];
+                self.data[Self::left(rover)] = tmp;
+                rover = Self::left(rover);
+            } else {
+                self.data[rover] = self.data[Self::right(rover)];
+                self.data[Self::right(rover)] = tmp;
+                rover = Self::right(rover);
+            }
+        }
+    }
 
+    fn enqueue(&mut self, elem: T) {
+        self.data.push(elem);
+        Self::fixup(self);
+    }
+
+    fn dequeue(&mut self) -> T{
+        let tmp:T = self.data[0].clone();
+        self.data[0] = self.data.remove(self.data.len()-1);
+        Self::fixdown(self);
+        return tmp
+    }
+
+    fn peek(self) -> T {
+        return self.data[0];
+    }
+
+    fn isEmpty(self) -> bool {
+        return self.data.is_empty();
+    }
+
+    fn size(self) -> usize {
+        return self.data.len();
+    }
 }
